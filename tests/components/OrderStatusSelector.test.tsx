7fc22126c5e2421e2bee6275a1758c53186fb415
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2024-10-22 10:10:05 -0600

    Testing OrderStatusSelector

--- tests/components/OrderStatusSelector.test.tsx
+++ tests/components/OrderStatusSelector.test.tsx
@@ -5,15 +5,20 @@ import userEvent from '@testing-library/user-event'
 
 describe('OrderStatusSelector', () => {
     const renderComponent = () => {
+    const onChange = vi.fn()
+
     render(
         <Theme>
-        <OrderStatusSelector onChange={vi.fn()}/>
+        <OrderStatusSelector onChange={ onChange }/>
         </Theme>
     )
 
     return {
         trigger: screen.getByRole('combobox'),
-        getOptions: () => screen.findAllByRole('option')
+        getOptions: () => screen.findAllByRole('option'),
+        getOption: (label: RegExp) => screen.findByRole('option', { name: label }),
+        user: userEvent.setup(),
+        onChange
     }
     }
 
@@ -23,14 +28,41 @@ describe('OrderStatusSelector', () => {
     })
 
     it('should render correct statuses', async () => {
-    const { trigger, getOptions } = renderComponent()
+    const { trigger, getOptions, user } = renderComponent()
 
-    const user = userEvent.setup()
     await user.click(trigger)
     
     const options = await getOptions()
     expect(options).toHaveLength(3)
     const labels = options.map(option => option.textContent)
     expect(labels).toEqual(['New', 'Processed', 'Fulfilled'])
+    })
+
+    it.each([
+        { label: /processed/i, value: 'processed'},
+        { label: /fulfilled/i, value: 'fulfilled'},
+    ])('should call onChange with $value when the $label option is selected', async ({ label, value }) => {
+    const { trigger, user, onChange, getOption } = renderComponent()
+    await user.click(trigger)
+
+    const option = await getOption(label)
+    await user.click(option)
+
+    expect(onChange).toHaveBeenCalledWith(value)
+    })
+
+    it("should call onChange with 'new' when the /new/i option is selected", async () => {
+    const { trigger, user, onChange, getOption } = renderComponent()
+    await user.click(trigger)
+
+    const fulfilledOption = await getOption(/fulfilled/i)
+    await user.click(fulfilledOption)
+    expect(onChange).toHaveBeenCalledWith('fulfilled')
+
+    await user.click(trigger)
+    const newOption = await getOption(/new/i)
+    await user.click(newOption)
+
+    expect(onChange).toHaveBeenCalledWith('new')
     })
 })
