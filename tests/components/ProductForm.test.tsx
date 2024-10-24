a6b094f85fc8cc3e43da40ac21dbf6e6ecc4e450
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2024-10-24 12:07:49 -0600

    Exercise testing initial data

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -1,23 +1,28 @@
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import ProductForm from '../../src/components/ProductForm'
-import { Category } from '../../src/entities'
+import { Category, Product } from '../../src/entities'
 import AllProviders from '../AllProviders'
 import { db } from '../mocks/db'
 
 describe('ProductForm', () => {
     const categories: Category[] = []
+    const products: Product[] = []
     
     beforeAll(() => {
         [1, 2].forEach(() => {
             const category = db.category.create()
             categories.push(category)
         })
+
+        const product = db.product.create({ categoryId: categories[0].id })
+        products.push(product)
     })
 
     afterAll(() => {
         const categoryIds = categories.map(c => c.id)
         db.category.deleteMany({ where: { id: { in: categoryIds }}})
+        db.product.delete({ where: { id: { equals: products[0].id }}})
     })
 
     it('should render form fields', async () => {
@@ -42,5 +47,16 @@ describe('ProductForm', () => {
         categories.forEach(product => {
             expect(screen.getByRole('option', { name: product.name }))
         })
+    })
+
+    it('should render initial data when editing a product', async () => {
+        const product = products[0]
+        render(<ProductForm onSubmit={vi.fn()} product={product}/>, { wrapper: AllProviders})
+
+        await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
+
+        expect(screen.getByPlaceholderText(/name/i)).toHaveValue(product.name)
+        expect(screen.getByPlaceholderText(/price/i)).toHaveValue(product.price.toString())
+        expect(screen.getByRole('combobox', { name: /category/i })).toHaveTextContent(categories[0].name)
     })
 })
